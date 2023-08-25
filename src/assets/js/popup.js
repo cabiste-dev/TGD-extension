c0eb4561d87149f396c3de89ccbd72e1867633c6
cabiste <ahmedjj354@gmail.com>
2023-08-25 20:04:38 +0100

    scuffy background service implementation

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -1,43 +1,29 @@
-import { SendClientEvent, SendMuteEvent } from "./TwitterApi.js";
+
 
 
 document.getElementById("button_mute").addEventListener("click", MuteButton_Clicked);
 // document.getElementById("button_test").addEventListener("click", Test);
 
 // Initialize some stuff on opening the popup
-var cookies;
-LoadTwitterCookies();
+
 PopulatePopup();
 
 /**
  * for debugging random stuff
 */
 async function Test() {
-    let gimmies = await GetAllGimmicks();
-    console.log(gimmies);
-    for (let i = 0; i < gimmies.length; i++) {
-        window.open(`https://twitter.com/${gimmies[i]}`);
-        await sleep(1000);
-    }
+    chrome.runtime.sendMessage('', {
+        type: 'count'
+    });
 }
 
 /**
  * mutes all the users
 */
 async function MuteButton_Clicked() {
-    let gimmicks = await GetAllGimmicks();
-    // we only need 1 uuid
-    let uuid = GenerateGuid();
-
-    for (let i = 0; i < gimmicks.length; i++) {
-        let gimmick = gimmicks[i];
-        // await SendClientEvent(cookies, uuid);
-        let muted = await SendMuteEvent(gimmick, cookies, uuid);
-        // sleep if the user was muted to not get blocked by the api (if that even happens)
-        if (muted) {
-            await sleep(1000);
-        }
-    }
+    chrome.runtime.sendMessage('', {
+        type: 'mute'
+    });
 }
 
 /**
@@ -46,46 +32,6 @@ async function MuteButton_Clicked() {
 // function BlockAll() {
 // }
 
-/**
- * Thanks to https://stackoverflow.com/a/39914235/12429279
- * @param {number} ms how many milliseconds to sleep (1000ms -> 1s)
- */
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-/**
- * Loads the twitter cookies into memory
- */
-function LoadTwitterCookies() {
-    chrome.cookies.getAll({ domain: "twitter.com" }, function (data) {
-        cookies = data;
-    });
-}
-
-/**
- * Fetches a list
- * @returns an array of usernames to block
- */
-async function GetAllGimmicks() {
-    let data = [];
-
-    await fetch("https://raw.githubusercontent.com/cabiste69/TGD-extension/main/GimmickList.txt").then((response) => response.text())
-        .then((json) => data = json.split("\n"));
-
-    return data;
-}
-
-/**
- * generates a random guid v4,
- * Thanks to https://stackoverflow.com/a/2117523/12429279
- * @returns a random GUID as string
- */
-function GenerateGuid() {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-}
 
 /**
  * Populates the popup with the names of the users to mute
@@ -114,4 +60,13 @@ async function PopulatePopup() {
         // add it to the list
         gimmicksListElement.appendChild(gimmickElement);
     }
+}
+
+async function GetAllGimmicks() {
+    let data = [];
+
+    await fetch("https://raw.githubusercontent.com/cabiste69/TGD-extension/main/GimmickList.txt").then((response) => response.text())
+        .then((json) => data = json.split("\n"));
+
+    return data;
 }
---
+++ src/background/background.js
@@ -0,0 +1,83 @@
+import {SendMuteEvent} from "../assets/js/TwitterApi.js";
+
+// chrome.runtime.onInstalled.addListener(function () {
+//     chrome.contextMenus.create({
+//         "id": "sampleContextMenu",
+//         "title": "Sample Context Menu",
+//         "contexts": ["selection"]
+//     });
+// });
+
+// thanks to https://github.com/moshfeu/chrome-extension-notification-demo
+chrome.runtime.onMessage.addListener(data => {
+    if (data.type === 'count') {
+        Count();
+    }
+    if (data.type === 'mute') {
+        MuteUsers();
+    }
+});
+var cookies;
+LoadTwitterCookies();
+
+async function Count() {
+    for (let i = 0; i < 50; i++) {
+        console.log(i);
+        await sleep(100);
+    }
+    alert("goob");
+}
+
+async function MuteUsers() {
+    let gimmicks = await GetAllGimmicks();
+    // we only need 1 uuid
+    let uuid = GenerateGuid();
+
+    for (let i = 0; i < gimmicks.length; i++) {
+        let gimmick = gimmicks[i];
+        // await SendClientEvent(cookies, uuid);
+        let muted = await SendMuteEvent(gimmick, cookies, uuid);
+        // sleep if the user was muted to not get blocked by the api (if that even happens)
+        if (muted) {
+            await sleep(1000);
+        }
+    }
+    alert("Finished muting all accounts");
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Loads the twitter cookies into memory
+ */
+function LoadTwitterCookies() {
+    chrome.cookies.getAll({ domain: "twitter.com" }, function (data) {
+        cookies = data;
+    });
+}
+
+/**
+ * Fetches a list
+ * @returns an array of usernames to block
+ */
+async function GetAllGimmicks() {
+    let data = [];
+
+    await fetch("https://raw.githubusercontent.com/cabiste69/TGD-extension/main/GimmickList.txt").then((response) => response.text())
+        .then((json) => data = json.split("\n"));
+
+    return data;
+}
+
+/**
+ * generates a random guid v4,
+ * Thanks to https://stackoverflow.com/a/2117523/12429279
+ * @returns a random GUID as string
+ */
+function GenerateGuid() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
